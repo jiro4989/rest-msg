33ee794c9a65cc5201592cd1c1b5e77eb039a179
jiro4989 <jiroron666@gmail.com>
2018-05-30 20:43:30 +0900

    関数にアラーム機能を追加した

--- js/standard/clock.js
+++ js/standard/clock.js
@@ -7,11 +7,29 @@
  * @param id 時計に設定する要素のID
  */
 function bindClock(id) {
-  let pad = (n) => ('0' + n).slice(-2);
-  let countTime = function() {
-    let date     = new Date();
-    let timeText = formatTime(date);
+  const pad = (n) => ('0' + n).slice(-2);
+  const countTime = function() {
+    const date     = new Date();
+    const timeText = formatTime(date);
     document.getElementById(id).innerHTML = timeText;
+    setTimeout(countTime, 500);
+  }
+  countTime();
+}
+
+/**
+ * 指定の時間にf関数を実行する。
+ * 検査間隔は0.5秒に一度
+ * @param targetDate 関数を実行させたい時間
+ * @param f 実行させたい関数
+ */
+function alertClock(targetDate, f) {
+  const countTime = function() {
+    const date = new Date();
+    if (targetDate.getTime() < date.getTime()) {
+      f();
+      return;
+    }
     setTimeout(countTime, 500);
   }
   countTime();
